f3c85c80d2e2b9d56bd675661c9e35a0b99c656c
SnO2WMaN <me@sno2wman.net>
2022-12-09 16:35:44 +0900

    nomore flake-compat

--- flake.nix
+++ flake.nix
@@ -12,10 +12,6 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
   outputs = {
--- shell.nix
+++ shell.nix
@@ -1,14 +1,1 @@
-(import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-      fetchTarball (with lock.nodes.flake-compat.locked; {
-        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-        sha256 = narHash;
-      })
-  )
-  {
-    src = ./.;
-  })
-.shellNix
+(builtins.getFlake ("git+file://" + toString ./.)).devShells.${builtins.currentSystem}.default
